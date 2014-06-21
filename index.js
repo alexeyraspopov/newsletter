f9baae68cf578825a0d8d7ea10513bed2b54cc96
Avenger7x <avenger7x13@gmail.com>
2014-06-21 12:05:22 +0300

    code

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your awesome code here
+module.exports = function(){
+	var listeners = [];
+
+	function unsubscribe(callback){
+		return function(){
+			var index = listeners.indexOf(callback);
+
+			if(index > -1){
+				listeners.splice(index, 1);
+			}
+		};
+	}
+
+	function noop(){}
+
+	return {
+		subscribe: function(callback){
+			if(listeners.indexOf(callback) < 0){
+				listeners.unshift(callback);
+
+				return unsubscribe(callback);
+			}
+
+			return noop;
+		},
+		publish: function(data){
+			var index = listeners.length;
+
+			while(--index >= 0){
+				listeners[index](data);
+			}
+		}
+	}
+};
