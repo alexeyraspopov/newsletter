7b0d47ce72ec7a13f00696c4d16d9c03915d1165
Alexey Raspopov <alexey.raspopov@datarobot.com>
2018-02-20 01:43:36 +0200

    update typings with new option

--- typings/newsletter.d.ts
+++ typings/newsletter.d.ts
@@ -1,7 +1,7 @@
 declare module 'newsletter' {
   export default class Newsletter {
     subscribers: Set<Function>;
-    subscribe(callback: Function): Function;
+    subscribe(callback: Function, options?: { once: boolean }): Function;
     unsubscribe(callback: Function): void;
     publish(data: any): void;
   }
