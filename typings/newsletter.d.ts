1b86ea5906849fab82ad09acd6b42c29966b8e9a
Alexey Raspopov <oleksii.raspopov@gmail.com>
2021-02-10 23:38:34 -0500

    use generic types

--- typings/newsletter.d.ts
+++ typings/newsletter.d.ts
@@ -1,8 +1,8 @@
-declare module 'newsletter' {
-  export default class Newsletter {
-    subscribers: Set<Function>;
-    subscribe(callback: Function, options?: { once: boolean }): Function;
-    unsubscribe(callback: Function): void;
-    publish(data: any): void;
-  }
+export default class Newsletter<Data> {
+  subscribe(callback: (Data) => void | Promise<void>): Subscription;
+  publish(data: Data): void;
+}
+
+class Subscription {
+  dispose(): void;
 }
