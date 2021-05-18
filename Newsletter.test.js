a087828ce218c4f98a3edec5f05d74454e7e74be
Alexey Raspopov <oleksii.raspopov@gmail.com>
2021-05-17 22:23:46 -0400

    definitive tests only

--- Newsletter.test.js
+++ Newsletter.test.js
@@ -1,52 +1,27 @@
 import { jest } from '@jest/globals';
 import { Newsletter } from './Newsletter';
 
-function createContext() {
+test('basic subscribers management cases', () => {
   let signal = new Newsletter();
   let value = 13;
   let listener = jest.fn();
-  return { signal, value, listener };
-}
 
-describe('Newsletter', () => {
-  it('should publish value', () => {
-    let { signal, value, listener } = createContext();
+  let subscriptionA = signal.subscribe(listener);
+  let subscriptionB = signal.subscribe(listener);
+  let subscriptionC = signal.subscribe(listener);
+  let subscriptionD = signal.subscribe(listener);
+  let subscriptionE = signal.subscribe(listener);
 
-    signal.subscribe(listener);
-    signal.publish(value);
+  signal.publish(value);
 
-    expect(listener).toHaveBeenCalledWith(value);
-  });
+  subscriptionA.dispose();
+  subscriptionE.dispose();
+  subscriptionC.dispose();
+  subscriptionB.dispose();
+  subscriptionD.dispose();
 
-  it('should publish to multiple listeners', () => {
-    let { signal, value, listener } = createContext();
+  signal.publish(value);
 
-    let subscriptionA = signal.subscribe(listener);
-    let subscriptionB = signal.subscribe(listener);
-    let subscriptionC = signal.subscribe(listener);
-    let subscriptionD = signal.subscribe(listener);
-    let subscriptionE = signal.subscribe(listener);
-
-    signal.publish(value);
-
-    subscriptionA.dispose();
-    subscriptionE.dispose();
-    subscriptionC.dispose();
-    subscriptionB.dispose();
-    subscriptionD.dispose();
-
-    signal.publish(value);
-
-    expect(listener).toHaveBeenCalledTimes(5);
-  });
-
-  it('should return subscription handler', () => {
-    let { signal, value, listener } = createContext();
-
-    let subscription = signal.subscribe(listener);
-    subscription.dispose();
-    signal.publish(value);
-
-    expect(listener).not.toHaveBeenCalled();
-  });
+  expect(listener).toHaveBeenCalledWith(value);
+  expect(listener).toHaveBeenCalledTimes(5);
 });
