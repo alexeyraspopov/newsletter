b2035c08f16efdb6ae81f93b19250865d23b313d
Alexey Raspopov <oleksii.raspopov@gmail.com>
2021-05-17 21:58:10 -0400

    remove unused scripts

--- scripts/build.sh
+++
@@ -1,3 +0,0 @@
-rollup --format cjs --config rollup.config.js modules/Newsletter.js > newsletter.js
-rollup --format es --config rollup.config.js modules/Newsletter.js > newsletter.module.js
-cp typings/newsletter.js.flow newsletter.js.flow
